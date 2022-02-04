673f3e01d6f3277b6053d51f2cafc76806f53728
willj4ps <89949187+willj4ps@users.noreply.github.com>
2022-02-04 14:17:18 -0300

    Add files via upload

---
+++ fundamentos_projetos/area_circulo_v1.py
@@ -0,0 +1,7 @@
+pi = 3.14159
+print('Cálculo da área da circunferência')
+print('PI x raio elevado ao quadrado')
+print('PI = 3,14159')
+raio = float(input('Qual o valor do raio? '))
+formula = pi * (raio ** 2)
+print(f'A área da circunferência é: {formula}')
